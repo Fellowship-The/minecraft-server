b6bfa2c2f1669f4c23f08aafb22c8560e0078d83
ROGUH Rivera Calzadillas <roguh@roguh.com>
2024-12-28 21:32:54 -0700

    updar

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-SERVER_DATA="${SERVER_DATA-./test_server_data}"
+ROOT="$(dirname $0)"
+BACKUP_SERVER_DATA="${BACKUP_SERVER_DATA-"$ROOT"/test_server_data}"
 
 set -x
-ROOT="$(dirname $0)"
 
 # Make a backup before starting the server. The compression can be slow. You
 # can safely cancel this command with CTRL-C.
-####### ./scripts/complicated-backup.sh "$SERVER_DATA"
+####### ./scripts/complicated-backup.sh "$BACKUP_SERVER_DATA"
 
-"$ROOT/scripts/backup.sh" "$SERVER_DATA"
+"$ROOT/scripts/backup.sh" "$BACKUP_SERVER_DATA"
 
 # Use docker compose to build the container and then run it.
 # `mc` is the service name in the docker-compose.yml file
