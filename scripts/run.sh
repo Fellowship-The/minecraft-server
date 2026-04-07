43c1dfd8aa937d3ed3a90e4a8c2a790b7bc36de0
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2026-04-06 23:59:34 -0600

    private.env

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,10 +5,14 @@ BACKUP_SERVER_DATA="${BACKUP_SERVER_DATA-"$ROOT/$MAP"}"
 
 set -x
 
+# Backup data
 "$ROOT/scripts/backup.sh" "$BACKUP_SERVER_DATA"
+
+# Load private config settings
+touch "$ROOT/private.env"
+set -a
+. "$ROOT/private.env"
 
 # Use docker compose to build the container and then run it.
 # `mc` is the service name in the docker-compose.yml file
-set -a
-. "$ROOT/private.env"
 docker compose -f $ROOT/docker-compose.yml up --build $@
