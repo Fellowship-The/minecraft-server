8ea738ebab16be163a29b7834514b17638600755
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2026-04-07 00:31:40 -0600

    adjust printing and shutdown process

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
-set -x
 MAP="${MAP:-IMPERIUS_FELINUS_server_data}"
 ROOT="$(dirname $(dirname $0))"
 BACKUP_SERVER_DATA="${BACKUP_SERVER_DATA-"$ROOT/$MAP"}"
 
+touch "$ROOT/private.env"
+set -a  # Load env vars globally
+# Load private config settings
+. "$ROOT/private.env"
+
 set -x
 # Backup data
 "$ROOT/scripts/backup.sh" "$BACKUP_SERVER_DATA"
-
-# Load private config settings
-touch "$ROOT/private.env"
-set -a
-. "$ROOT/private.env"
 
 # Use docker compose to build the container and then run it.
 # `mc` is the service name in the docker-compose.yml file
--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -2,5 +2,4 @@
 
 set -x
 docker exec minecraft-server-mc-1 mc-send-to-console stop
-sleep 5
-docker stop --timeout 600 minecraft-server-mc-1
+docker stop minecraft-server-mc-1
