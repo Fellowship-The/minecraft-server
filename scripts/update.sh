e8c0e88c98d6efb3e8b09d563ed780007b0a8c7b
Felina Rivera Calzadillas (ROGUH) <felina.rivera@powerflex.com>
2026-04-07 01:07:05 -0600

    restart-right

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,15 +3,17 @@ PARENT="$(realpath "$(dirname "$0")"/../)"
 cd "$PARENT"
 
 set -x
+
 git fetch origin
 # Compare the local branch (main) with its remote tracking branch (origin/main)
 if [ $(git rev-parse HEAD) = $(git rev-parse @{u}) ]; then
     echo "No updates available. $(date)"
-    exit 1
 else
     git merge
     echo "Updates pulled successfully."
     echo "Restarting now. $(date)"
-    ./scripts/restart.sh
+    ./scripts/shutdown.sh
 fi
 
+# Make sure server is running no matter what
+./scripts/run.sh
